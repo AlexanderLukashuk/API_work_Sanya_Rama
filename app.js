a09f518cb485f53569542f6d68154d01c113669d
Саня <aleksandrlukasuk@MacBook-Air-Aleksandr.local>
2022-05-11 06:21:47 +0600

    Added procfile and git ignore

--- app.js
+++ app.js
@@ -2,22 +2,42 @@ const express = require('express');
 const app = express();
 const bodyparser = require('body-parser')
 const ejs = require('ejs')
+const authRouter = require("./routers/authRouter")
+const mongoose = require('mongoose');
 
-const PORT = 3000
+// const PORT = 3000
+const url = "mongodb+srv://maulerr:Aitu2021!@backend.koyk6.mongodb.net/backend?retryWrites=true&w=majority";
+
+let port = process.env.PORT
+if (port == null || port == "") {
+    port = 3000;
+}
+
 
 app.set("view engine", "ejs")
-
 app.use('/static', express.static('static'))
 app.use(bodyparser.urlencoded({extended: true}))
+app.use(bodyparser.json())
 
-app.use("/", require("./routers/index"))
-app.use("/", require("./routers/about"))
-app.use("/", require("./routers/workpage"))
-app.use("/", require("./routers/login"))
-app.use("/", require("./routers/reg"))
-app.use("/", require("./routers/workouts"))
+app.use(express.json())
 
-app.listen(PORT, () => {
-    console.log(`App was launched on http://localhost:${PORT}`)
-})
+try {
+    mongoose.connect(url);
+
+    app.use('/auth', authRouter);
+    app.use("/", require("./routers/index"))
+    app.use("/", require("./routers/about"))
+    app.use("/", require("./routers/workpage"))
+    app.use("/", require("./routers/login"))
+    app.use("/", require("./routers/reg"))
+    app.use("/", require("./routers/adminRouter"))
+    // app.use("/", require("./routers/workouts"))
+
+    app.listen(port, () => {
+        console.log(`App was launched on http://localhost:${port}`)
+    })
+} catch (e) {
+    console.log("Something went wrong!" +
+        "" + e)
+}
 
--- routers/reg.js
+++ routers/reg.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 const path = require("path")
 const ejs = require('ejs')
+const bodyparser = require('body-parser')
+const authCtrl = require('../controllers/authController')
+
+const {check} = require("express-validator");
 
 router
-    .route("/reg")
+    .route('/reg')
     .get((req, res) => res.render(path.resolve("pages/reg.ejs")))
-    .post((req, res) => res.render(path.resolve("pages/reg.ejs")));
+    .post(authCtrl.registration)
 module.exports = router;
